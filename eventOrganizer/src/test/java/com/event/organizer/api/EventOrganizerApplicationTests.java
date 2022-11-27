6ac1e0c89d95e1c298bf74d1a1556cd3323d7712
Kristiyan Selveliev <kristiyan_selveliev@epam.com>
2022-11-27 18:54:42 +0200

    Add empty java project

---
+++ eventOrganizer/src/test/java/com/event/organizer/api/EventOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.organizer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
