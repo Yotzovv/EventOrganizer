2e2500304953befb90beeefb39f857bb4de2241f
kstoyanov <kstoyanov@appolica.com>
2022-12-03 18:19:01 +0200

    Implement EventService.getAllPendingEvents

--- eventOrganizer/src/main/java/com/event/organizer/api/service/EventService.java
+++ eventOrganizer/src/main/java/com/event/organizer/api/service/EventService.java
@@ -2,11 +2,12 @@ package com.event.organizer.api.service;
 
 import com.event.organizer.api.exception.EventOrganizerException;
 import com.event.organizer.api.model.Event;
-import com.event.organizer.api.model.dto.EventRequestDto;
 import com.event.organizer.api.repository.EventRepository;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -37,5 +38,12 @@ public class EventService {
             throw new EventOrganizerException("Event does not exist");
         }
         eventRepository.delete(event);
+    }
+
+    public List<Event> getAllPendingEvents() {
+        return this.findAll()
+                .stream()
+                .filter(event -> Objects.equals(event.getStatus(), Event.NONE_STATUS))
+                .collect(Collectors.toList());
     }
 }
