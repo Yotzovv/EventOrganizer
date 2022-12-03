d72e705d29509a5230f99e8c692a21dd32bc5601
ivyo <ivyo@WIN-0OVOR0CIS69>
2022-12-04 01:35:19 +0200

    Add Edit Event Dialog

--- eventorganizer.ui/src/app/home-page/home-page.component.ts
+++ eventorganizer.ui/src/app/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { RequestService } from './../request/request.service';
 import { Component } from '@angular/core';
 import { EventDto } from '../types/event.type';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogAnimationsExampleDialog } from '../create-event-dialog/create-event-dialog.component'
 
 @Component({
   selector: 'home-page',
@@ -8,11 +10,17 @@ import { EventDto } from '../types/event.type';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
+  createEventDialog: MatDialogRef<DialogAnimationsExampleDialog>;
+
   public allEvents: EventDto[] = [];
 
-  constructor(requestService: RequestService) {
+  constructor(requestService: RequestService, private dialogModel: MatDialog) {
     requestService.getAllEvents$().subscribe((res) => {
       this.allEvents = res;
     });
+  }
+
+  openCreateEventDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.createEventDialog = this.dialogModel.open(DialogAnimationsExampleDialog);
   }
 }
