81c4d856ce2ea90996220a1a61a836c1245b7669
Igor Neshovski <unskilledbg@gmail.com>
2022-12-12 19:52:05 +0200

    edited the correct files

--- eventOrganizer/src/test/java/com/event/organizer/api/EventOrganizerApplicationTests.java
+++ eventOrganizer/src/test/java/com/event/organizer/api/EventOrganizerApplicationTests.java
@@ -10,4 +10,54 @@ class EventOrganizerApplicationTests {
 	void contextLoads() {
 	}
 
+	@test
+	public void testEditAccount_withValidInput_updatesAccount() {
+		// Arrange
+		AppUser editedUser = new AppUser();
+		editedUser.setEmail("test@example.com");
+
+		// Set up mocked user repository to return the edited user when findByEmail is called
+		UserRepository mockedUserRepository = mock(UserRepository.class);
+		when(mockedUserRepository.findByEmail(editedUser.getEmail())).thenReturn(Optional.of(editedUser));
+
+		// Set up class under test and inject mocked user repository
+		EditAccountService editAccountService = new EditAccountService();
+		editAccountService.setUserRepository(mockedUserRepository);
+
+		// Act
+		editAccountService.EditAccount(editedUser);
+
+		// Assert
+		verify(mockedUserRepository).save(editedUser);
+	}
+
+	@test(expected = UsernameNotFoundException.class)
+	public void testEditAccount_withNullInput_throwsException() {
+		// Arrange
+		AppUser editedUser = null;
+
+		// Set up class under test
+		EditAccountService editAccountService = new EditAccountService();
+
+		// Act
+		editAccountService.EditAccount(editedUser);
+	}
+
+	@test(expected = UsernameNotFoundException.class)
+	public void testEditAccount_withNonexistentEmail_throwsException() {
+		// Arrange
+		AppUser editedUser = new AppUser();
+		editedUser.setEmail("test@example.com");
+
+		// Set up mocked user repository to return empty Optional when findByEmail is called
+		UserRepository mockedUserRepository = mock(UserRepository.class);
+		when(mockedUserRepository.findByEmail(editedUser.getEmail())).thenReturn(Optional.empty());
+
+		// Set up class under test and inject mocked user repository
+		EditAccountService editAccountService = new EditAccountService();
+		editAccountService.setUserRepository(mockedUserRepository);
+
+		// Act
+		editAccountService.EditAccount(editedUser);
+	}
 }
