759fd024e5b00cf7d5ae9a111a4b1d93cc46c4bd
Igor Neshovski <unskilledbg@gmail.com>
2022-12-12 19:45:30 +0200

    *NOT TESTED* added test cases *NOT TESTED*

--- eventOrganizer/src/main/java/com/event/organizer/api/EventOrganizerApplication.java
+++ eventOrganizer/src/main/java/com/event/organizer/api/EventOrganizerApplication.java
@@ -1,14 +1,63 @@
 package com.event.organizer.api;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
-public class EventOrganizerApplication {
+@SpringBootTest
+class EventOrganizerApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventOrganizerApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
 
+	@test
+	public void testEditAccount_withValidInput_updatesAccount() {
+		// Arrange
+		AppUser editedUser = new AppUser();
+		editedUser.setEmail("test@example.com");
+
+		// Set up mocked user repository to return the edited user when findByEmail is called
+		UserRepository mockedUserRepository = mock(UserRepository.class);
+		when(mockedUserRepository.findByEmail(editedUser.getEmail())).thenReturn(Optional.of(editedUser));
+
+		// Set up class under test and inject mocked user repository
+		EditAccountService editAccountService = new EditAccountService();
+		editAccountService.setUserRepository(mockedUserRepository);
+
+		// Act
+		editAccountService.EditAccount(editedUser);
+
+		// Assert
+		verify(mockedUserRepository).save(editedUser);
+	}
+
+	@test(expected = UsernameNotFoundException.class)
+	public void testEditAccount_withNullInput_throwsException() {
+		// Arrange
+		AppUser editedUser = null;
+
+		// Set up class under test
+		EditAccountService editAccountService = new EditAccountService();
+
+		// Act
+		editAccountService.EditAccount(editedUser);
+	}
+
+	@test(expected = UsernameNotFoundException.class)
+	public void testEditAccount_withNonexistentEmail_throwsException() {
+		// Arrange
+		AppUser editedUser = new AppUser();
+		editedUser.setEmail("test@example.com");
+
+		// Set up mocked user repository to return empty Optional when findByEmail is called
+		UserRepository mockedUserRepository = mock(UserRepository.class);
+		when(mockedUserRepository.findByEmail(editedUser.getEmail())).thenReturn(Optional.empty());
+
+		// Set up class under test and inject mocked user repository
+		EditAccountService editAccountService = new EditAccountService();
+		editAccountService.setUserRepository(mockedUserRepository);
+
+		// Act
+		editAccountService.EditAccount(editedUser);
+	}
 }
